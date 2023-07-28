21bc38c2ffa68c8f02dd3e264ff065834a3ff3e6
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-07-28 19:53:23 +0530

    feat - started graph ql learning

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
-import  bodyParser from "body-parser";
+import bodyParser from "body-parser";
 import cors from "cors";
-import {expressMiddleware} from "@apollo/server/express4"
+import { expressMiddleware } from "@apollo/server/express4";
+import axios from "axios";
 
-const startServer = async() => {
-    const app = express();
-    const server = new ApolloServer({
-        typeDefs:`
-            type Todo{
+const startServer = async () => {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
+            type Users {
+                id: ID!
+                name: String
+                email : String
+                phone: Int,
+                website: String,
+            }
+            type Todo {
                 id: ID!
                 title : String!
                 completed : Boolean
@@ -17,16 +25,20 @@ const startServer = async() => {
                 getTodos : [Todo]
             }
         `,
-        resolvers:{}
-    });
+    resolvers: {
+      Query: {
+        getTodos: async() => (await axios.get("https://jsonplaceholder.typicode.com/todos")).data,
+      }
+    },
+  });
 
-    app.use(bodyParser.json());
-    app.use(cors());
+  app.use(bodyParser.json());
+  app.use(cors());
 
-    await server.start();
+  await server.start();
 
-    app.use("/graphQl",expressMiddleware(server));
+  app.use("/graphql", expressMiddleware(server));
 
-    app.listen(3000,()=>console.log("server started on PORT 8000..."))
+  app.listen(3000, () => console.log("server started on PORT 3000..."));
 };
 startServer();
